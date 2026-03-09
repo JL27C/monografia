77a48750e63be0068210b2e534515ebf18fc08cb
JL27C <joelcasanova.diaz@gmail.com>
2026-03-08 19:11:49 -0500

    primera

---
+++ Mustra/js/chatbot.js
@@ -0,0 +1,20 @@
+const areaRespuesta= document.getElementById("respuesta")
+const imagen = document.getElementById("imagenchatbot") 
+
+
+function decirHola(){
+    areaRespuesta.value= "Hola!,Me alegra saludarte";
+    imagen.src="./img/chatbot/saludo1.jpg";
+ 
+}
+ 
+function comoEstas(){
+    areaRespuesta.value=" Estoy bien,funcionando correctamente y listo para trabajar";
+    imagen.src="./img/chatbot/saludo2.jpg";   
+}
+ 
+function comoTeSientes(){
+    areaRespuesta.value="Mesiento de maravilla ycon ganasde aprenderbiologia";
+    imagen.src="./img/chatbot/saludo3.jpg";
+}
+ 
